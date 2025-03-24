6a076692a99d1ae0a80753322e0c7b904c92d807
thereto <827742363@qq.com>
2025-03-24 11:01:53 +0800

    Update diymo.sh

--- ax6/diymo.sh
+++ ax6/diymo.sh
@@ -4,7 +4,7 @@ if [ ! -d "/var/run/homeproxy" ]; then
 fi
 
 #/etc/init.d/kodexplorer restart
-/etc/init.d/zerotier restart
+#/etc/init.d/zerotier restart
 #/etc/init.d/samba4 restart
 #/etc/init.d/ttyd restart
 /etc/init.d/homeproxy restart
